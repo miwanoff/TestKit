16d1bfe5a1bc8ce01414be26b515fe3456a446b5
Marina Ivanova <marina.iva.noffa@gmail.com>
2019-10-18 18:28:58 +0300

    delete comments

--- test-json/js_jq.js
+++ test-json/js_jq.js
@@ -9,42 +9,7 @@ $(document).ready(function () {
                     $("div#"+i+".col.s12").append("<p>" + k + "</p>");
                     $("div#"+i+".col.s12").append("<p>" + v + "</p>");
                 });
-            });
-            //     // указываем url и функцию обратного вызова;
-            //     let tests = [];
-            //     let qstr = "";
-
-            //     let group_number = 0;
-            //     let test_id = 0;
-            //     for (let key in data) {
-            //         let answ_number = 0;
-            //         qstr +=
-            //             '<div class="row tests z-depth-2"><div class="col s12">' +
-            //             "<h4>" +
-            //             key +
-            //             "</h4>";
-            //         for (let k in data[key]) {
-            //             qstr +=
-            //                 "<p> <input name='group" +
-            //                 group_number +
-            //                 "' type='radio' id='test" +
-            //                 test_id +
-            //                 "' /><label for='test" +
-            //                 test_id +
-            //                 "'>" +
-            //                 k +
-            //                 "</label></p>";
-            //             answ_number++;
-            //             test_id++;
-            //         }
-            //         qstr += "</div></div>";
-            //         group_number++;
-            //     }
-            //    // tests.push(qstr);
-            //     $("<form>", {
-            //         class: "my-new-list",
-            //         html: qstr
-            //     }).appendTo(".tests");
+            });           
         });
     });
 });
